4efcb7db1fb3b546e6a643765e762fe5f4f72e37
Jean Carlo <jeansanto2002@gmail.com>
2024-07-10 11:30:55 -0300

    Incluindo validações e testes

---
+++ src/test/java/br/com/alura/codechella/domain/entities/UsuarioTest.java
@@ -0,0 +1,42 @@
+package br.com.alura.codechella.domain.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class UsuarioTest {
+    @Test
+    public void naoDeveCadastrarUsuarioComCpfInvalido() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Usuario("12345678", "Fulano", LocalDate.parse("2002-06-15"), "fulano@gmail.com");
+        });
+    }
+
+    @Test
+    public void deveCadastrarUsuarioComCpfValido() {
+        Usuario usuario = new Usuario("123.456.789-01", "Fulano", LocalDate.parse("2002-06-15"), "fulano@gmail.com");
+        Assertions.assertTrue(true, usuario.getCpf());
+    }
+
+    @Test
+    public void naoDeveCadastrarUsuarioComNomeVazio() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Usuario("123.456.789-01", "", LocalDate.parse("2002-06-15"), "fulano@gmail.com");
+        });
+    }
+
+    @Test
+    public void naoDeveCadastrarUsuarioComDataNascimentoInvalida() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Usuario("123.456.789-01", "Fulano", LocalDate.parse("2025-06-15"), "fulano@gmail.com");
+        });
+    }
+
+    @Test
+    public void naoDeveCadastrarUsuarioComEmailInvalido() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Usuario("123.456.789-01", "Fulano", LocalDate.parse("2002-06-15"), "fulanogmail.com");
+        });
+    }
+}
